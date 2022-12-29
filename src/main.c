4488534dcf156cde749e04f24ed1930b4b975882
harr1424 <harrington59802@protonmail.com>
2022-12-28 17:56:20 -0700

    static allocation working as expected

--- src/main.c
+++ src/main.c
@@ -5,13 +5,23 @@
 #include <string.h>
 #include <limits.h>
 #include <unistd.h>
-#include "fs_info.h"
 
 // size of array used to contain filesystem entries
 const size_t fs_info_arr_size = 10;
 
+/*
+    A struct to contain the name of a filesystem entry and its size in bytes.
+    An array of this type will be used to catalog all filesystem entries for
+    the directory specified as command line argument.
+*/
+typedef struct FS_Info
+{
+    char name[PATH_MAX];
+    long long size;
+} FS_Info;
+
 // global pointer to FS_Info array
-FS_Info *fs_info_arr[fs_info_arr_size];
+FS_Info fs_info_arr[fs_info_arr_size];
 
 // used to sort fs_entries array descending in terms of entry size
 static int compare(const void *a, const void *b)
@@ -19,24 +29,24 @@ static int compare(const void *a, const void *b)
     const struct FS_Info *entryA = (FS_Info *)a;
     const struct FS_Info *entryB = (FS_Info *)b;
 
-    return (entryB->size - entryA->size) - (entryA->size - entryB->size);
+return entryB->size - entryA->size;
 }
 
 /*
-Iterates over an array of FS_Info structs and returns a pointer to the struct 
-having the smallest size member. 
+Iterates over an array of FS_Info structs and returns a pointer to the struct
+having the smallest size member.
 */
-FS_Info *get_smallest_entry(FS_Info **entries)
+FS_Info *get_smallest_entry(FS_Info *entries)
 {
-    long long smallest = entries[0]->size;
-    FS_Info *target;
+    long long smallest = entries[0].size;
+    FS_Info *target = &entries[0];
 
-    for (int i = 1; i < fs_info_arr_size * sizeof(FS_Info); i += sizeof(FS_Info))
+    for (int i = 1; i < fs_info_arr_size; i++)
     {
-        if (entries[i]->size < smallest)
+        if (entries[i].size < smallest)
         {
-            smallest = entries[i]->size;
-            target = entries[i];
+            smallest = entries[i].size;
+            target = &entries[i];
         }
     }
     return target;
@@ -46,7 +56,7 @@ FS_Info *get_smallest_entry(FS_Info **entries)
 Add entires to the array. If the array is full, use the above function to find the
 struct having the smallest file size, and if the current file size is larger, replace it.
 */
-void update_fs_info_arr(char *path)
+void update_fs_info_arr(char *path) 
 {
     static int items_added = 0;
 
@@ -55,18 +65,18 @@ void update_fs_info_arr(char *path)
     {
         if (items_added < fs_info_arr_size) // if array capacity will not be exceeded
         {
-            strncpy(fs_info_arr[items_added]->name, path, sizeof(fs_info_arr[items_added]->name)/sizeof(fs_info_arr[items_added]->name[0]) - 1);
-            fs_info_arr[items_added]->size = st.st_size;
+            strncpy(fs_info_arr[items_added].name, path, PATH_MAX);
+            fs_info_arr[items_added].size = st.st_size;
 
             items_added++;
         }
-        else 
+        else
         // find entry having the smallest size and replace it with the current entry if it is larger
         {
-            FS_Info *smallest = get_smallest_entry(fs_info_arr); 
+            FS_Info *smallest = get_smallest_entry(fs_info_arr);
             if (st.st_size > smallest->size)
             {
-                strncpy(smallest->name, path, sizeof(smallest->name)/sizeof(smallest->name[0]) - 1);
+                strncpy(smallest->name, path, PATH_MAX);
                 smallest->size = st.st_size;
             }
         }
@@ -97,15 +107,12 @@ void walk(const char *currDir)
         }
 
         char path_to_entry[PATH_MAX];
-        snprintf(path_to_entry, sizeof(path_to_entry) - 1, "%s/%s", currDir, entry->d_name);
-        path_to_entry[sizeof(path_to_entry) - 1] = '\0';
+        snprintf(path_to_entry, sizeof(path_to_entry), "%s/%s", currDir, entry->d_name);
 
-        // use path_to_entry to call stats on the entry
-        update_fs_info_arr(path_to_entry); // fs_entries will only be used on first call
+        update_fs_info_arr(path_to_entry);
 
         if (entry->d_type == DT_DIR)
         {
-            // recursively visit subdirectories
             walk(path_to_entry);
         }
     }
@@ -118,29 +125,31 @@ int main(int argc, char *argv[])
     // a char array to hold a filesystem path
     char target_dir[PATH_MAX];
 
-    if (argc > 2)
-    { // more than one argument passed at invocation
-        char *error_message = "Usage: %s <target directory>\n", argv[0];
-        fprintf(stderr, "%s", error_message);
-        return EXIT_FAILURE;
-    }
+    // if (argc > 2)
+    // { // more than one argument passed at invocation
+    //     char *error_message = "Usage: %s <target directory>\n", argv[0];
+    //     fprintf(stderr, "%s", error_message);
+    //     return EXIT_FAILURE;
+    // }
 
-    else if (argc == 1)
-    { // no argument passed at invocation, default to current working directory
-        if (getcwd(target_dir, sizeof(target_dir)) != NULL)
-        {
-            printf("Defaulting to current directory\n");
-        }
-        else
-        {
-            perror("Unable to detect current working directory, try passing a directory as command line argument");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    { // exactly one path specified as argument at invocation
-        strncpy(target_dir, argv[1], PATH_MAX);
-    };
+    // else if (argc == 1)
+    // { // no argument passed at invocation, default to current working directory
+    //     if (getcwd(target_dir, sizeof(target_dir)) != NULL)
+    //     {
+    //         printf("Defaulting to current directory\n");
+    //     }
+    //     else
+    //     {
+    //         perror("Unable to detect current working directory, try passing a directory as command line argument");
+    //         exit(EXIT_FAILURE);
+    //     }
+    // }
+    // else
+    // { // exactly one path specified as argument at invocation
+    //     strncpy(target_dir, argv[1], PATH_MAX);
+    // };
+
+    strncpy(target_dir, argv[1], PATH_MAX);
 
     printf("Finding the %zu largest files in: %s\n", fs_info_arr_size, target_dir);
 
@@ -153,7 +162,7 @@ int main(int argc, char *argv[])
     // output ten largest files found
     for (int i = 0; i < fs_info_arr_size; i++)
     {
-        printf("%s\t%lld\n", fs_info_arr[i]->name, fs_info_arr[i]->size);
+        printf("%s\t%lld\n", fs_info_arr[i].name, fs_info_arr[i].size);
     }
 
     return EXIT_SUCCESS;
